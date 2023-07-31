2f53064ee388bd823ece2f09c0ece65ce0c3df68
kylodread <joshuanichols2204@gmail.com>
2023-07-31 14:56:49 +0200

    Ad units with the Ad ID created.

--- lib/utils/AdCommon.dart
+++ lib/utils/AdCommon.dart
@@ -4,35 +4,35 @@ import 'Config.dart';
 /// Open screen ad id
 String openAppAdId = Platform.isAndroid
     ? Config.isDebug
-        ? 'ca-app-pub-3940256099942544/3419835294'
-        : 'ca-app-pub-3940256099942544/3419835294'
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248'
     : Config.isDebug
-        ? 'ca-app-pub-3940256099942544/3419835294'
-        : 'ca-app-pub-3940256099942544/3419835294';
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248';
 
 /// task interstitial
 String taskAdId = Platform.isAndroid
     ? Config.isDebug
-        ? 'ca-app-pub-3940256099942544/1033173712'
-        : 'ca-app-pub-3940256099942544/1033173712'
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248'
     : Config.isDebug
-        ? 'ca-app-pub-3940256099942544/1033173712'
-        : 'ca-app-pub-3940256099942544/1033173712';
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248';
 
 /// Task Interstitial Rewarded Ads
 String taskRewardAdId = Platform.isAndroid
     ? Config.isDebug
-        ? 'ca-app-pub-3940256099942544/5354046379'
-        : 'ca-app-pub-3940256099942544/5354046379'
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248'
     : Config.isDebug
-        ? 'ca-app-pub-3940256099942544/5354046379'
-        : 'ca-app-pub-3940256099942544/5354046379';
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248';
 
 /// Home Banner
 String homeBannerAd = Platform.isAndroid
     ? Config.isDebug
-        ? 'ca-app-pub-3940256099942544/6300978111'
-        : 'ca-app-pub-3940256099942544/6300978111'
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248'
     : Config.isDebug
-        ? 'ca-app-pub-3940256099942544/6300978111'
-        : 'ca-app-pub-3940256099942544/6300978111';
+        ? 'ca-app-pub-1411450732913265~7528190248'
+        : 'ca-app-pub-1411450732913265~7528190248';
